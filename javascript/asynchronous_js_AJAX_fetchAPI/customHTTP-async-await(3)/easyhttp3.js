828eba7217550a9372862449cc3f936535f5cd1d
Midha <midhatahirkhan2011@gmail.com>
2020-07-03 20:30:41 +0500

    make custom library with async await

---
+++ javascript/asynchronous_js_AJAX_fetchAPI/customHTTP-async-await(3)/easyhttp3.js
@@ -0,0 +1,51 @@
+/*
+EasyHttp Library
+Library for making HTTP requests
+@version 3.0.0 (using async await)
+*/
+
+class EasyHTTP {
+    // Make an HTTP GET request
+    async get(url){
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
+    }
+    //Make an HTTP POST REQUEST
+    async post(url,data){
+        const response = await fetch(url,{
+        method: 'POST',
+        headers : {
+            'Content-type' : 'application/json'
+        },
+        body : JSON.stringify(data)
+        });
+        const resData = await response.json();
+        return resData;
+    }
+
+    // Make an HTTP PUT REQUEST
+    async put(url,data){
+        const response = await fetch(url,{
+            method: 'PUT',
+            headers : {
+                'Content-type' : 'application/json'
+            },
+            body : JSON.stringify(data)
+        })
+        const resData = await response.json();
+        return resData;
+    }
+
+    //Make an HTTP DELETE REQUEST
+    async delete(url){  
+        const response = await fetch(url,{
+            method: 'DELETE',
+            headers : {
+                'Content-type' : 'application/json'
+            }
+        });
+        const resData = await 'Deleted';
+        return resData;
+    }
+}
